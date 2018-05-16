ddff88bff1982c13bcd4cf81f358fda6f20b0d08
Darryll Robinson <darryll@stillproud.com>
2018-05-16 16:59:29 +0200

    Working on the library tags

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,26 +3,20 @@ import React, { Component } from 'react';
 //import {Cloudinary, Configuration, Transformation, Util} from 'cloudinary-core';
 //import Video from './Cloudinary/Video';
 import Nav from './Nav';
-import Video from 'cloudinary-video-player';
 import cloudinary from "cloudinary-core";
 import axios from 'axios';
 
 class Screen extends Component {
+  constructor(props) {
+    super(props);
 
-  state = {
-    videos: []
-  };
+    this.state = {
+      tag: 'retail'
+    };
 
-  getVideos() {
-    axios.get('https://res.cloudinary.com/flycrow/video/list/fcm.json')
-          .then(res => {
-            console.log(res.data.resources);
-            this.setState({ videos: res.data.resources.splice(0,12)});
-    });
   }
 
   componentDidMount() {
-    //this.getVideos();
     // this.getIPstack(); // For when we want the device location info
     this.getPlaylist();
   }
@@ -33,18 +27,13 @@ class Screen extends Component {
       playedEventTimes: [1]
     });
 
-    player.playlistByTag('rpns', {
+    player.playlistByTag(this.state.tag, {
       //sourceParams: { angle: 13 },
       autoAdvance: 0,
       repeat: true
     }).then(function(player)
       {
-        //const divElem = document.querySelector("div#playlist-data");
-        const list = player.playlist().list().map(function(source) {
-          return source.publicId()
-        }).join(', ');
-
-        //divElem.innerText = 'Playlist: ' + list;
+        console.log('Playlist loaded');
       });
 
       // Fighting to get the stupid thing to play in full screen without intervention
@@ -63,22 +52,19 @@ class Screen extends Component {
 
   render() {
 
-    const { videos } = this.state;
-
     return (
       <div>
         <Nav />
-        <h3 className="text-center">Content Library</h3>
+        <h3 className="text-center">Screen Display</h3>
         <hr/>
 
         <div className="cloudPlayer">
           <video
             id="example-player"
-            //controls
-            //autoPlay
+            controls
+            autoPlay
             className="cld-video-player cld-video-player-skin-dark">
           </video>
-          {/*<div className="playlist-data"></div>*/}
         </div>
       </div>
     );
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 
 class Upload extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tag: ''
+    };
+
+    this.handleTagChange = this.handleTagChange.bind(this);
+  }
+
+  handleTagChange(event) {
+    // updating [tags] in state to send with upload request
+    this.setState({ tag: event.target.value });
+    console.log('tag: ', this.state.tag);
+  };
 
   uploadWidget = () => {
     window.cloudinary.openUploadWidget(
-      { cloud_name: 'flycrow',
+      { //cloud_name: 'flycrow',
         upload_preset: 'ubx3ytwg',
-        tags: ['rpns'],
-        sources: ['local', 'url', 'google_photos', 'facebook', 'image_search']
+        tags: this.state.tag,
+        sources: ['local', 'url']
       },
       function(error, result) {
           console.log("This is the result of the last upload", result);
       });
-  }
+  };
 
   render() {
 
@@ -25,6 +40,7 @@ class Upload extends Component {
 
         <div className="col-sm-12">
           <div className="jumbotron text-center">
+            <input placeholder="Content Tag" onChange={this.handleTagChange} />
             <button onClick={this.uploadWidget} className="btn btn-lg btn-info">Upload Content</button>
           </div>
         </div>
